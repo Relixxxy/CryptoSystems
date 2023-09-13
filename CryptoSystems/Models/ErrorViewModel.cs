c9d7fa8c56bc9c5064ab1a97102d1e3d69b68d4b
Vladyslav Pavlenko <vladyslav.pavlenko@nixs.com>
2023-09-13 21:33:43 +0300

    Add project files.

---
+++ CryptoSystems/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CryptoSystems.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
