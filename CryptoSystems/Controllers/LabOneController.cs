fd398faea4eabba7df0b63b449fec4fef63074a6
Vladyslav Pavlenko <vladyslav.pavlenko@nixs.com>
2023-09-14 23:26:27 +0300

    Added endpoints, styles and jquery for encription methods

--- CryptoSystems/Controllers/LabOneController.cs
+++ CryptoSystems/Controllers/LabOneController.cs
@@ -1,4 +1,5 @@
-﻿using CryptoSystems.Services.Interfaces;
+﻿using CryptoSystems.Services;
+using CryptoSystems.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CryptoSystems.Controllers;
@@ -6,15 +7,35 @@ namespace CryptoSystems.Controllers;
 public class LabOneController : Controller
 {
     private readonly ILaboratoryWorkService _labService;
+    private readonly LabOneCryptoService _labCryptoService;
 
-    public LabOneController(ILaboratoryWorkService labService)
+    public LabOneController(
+        ILaboratoryWorkService labService,
+        LabOneCryptoService labCryptoService)
     {
         _labService = labService;
+        _labCryptoService = labCryptoService;
     }
 
     public async Task<IActionResult> Index(int id)
     {
         var lab = await _labService.GetByIdAsync(id);
         return View(lab);
+    }
+
+    [HttpPost]
+    public ActionResult Encrypt(string inputText)
+    {
+        string encryptedText = _labCryptoService.Encrypt(inputText);
+
+        return Content(encryptedText);
+    }
+
+    [HttpPost]
+    public ActionResult Decrypt(string inputText)
+    {
+        string decryptedText = _labCryptoService.Decrypt(inputText);
+
+        return Content(decryptedText);
     }
 }
